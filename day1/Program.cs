256d5563f6f51a04dd923f0cdb79dc63b72cdb2c
Dave Clark <David.jb.clark@icloud.com>
2022-12-01 09:06:23 +0000

    day1

---
+++ day1/Program.cs
@@ -0,0 +1,17 @@
+﻿await File.ReadAllLinesAsync("input.txt").ContinueWith(t =>
+{
+    int current = 0;
+    var max = new[] { 0, 0, 0 };
+    foreach (var line in t.Result)
+    {
+        if (!string.IsNullOrWhiteSpace(line))
+        {
+            current += int.Parse(line);
+            continue;
+        }
+        max[0] = current > max[0] ? current : max[0];
+        max = max.OrderBy(i => i).ToArray();
+        current = 0;
+    }
+    Console.WriteLine(max.Sum()); ;
+});
