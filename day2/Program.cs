33ef94041b63ccca5898615ac2c45a16376c88d0
Dave Clark <David.jb.clark@icloud.com>
2022-12-02 09:36:39 +0000

    day2

---
+++ day2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+var watch = Stopwatch.StartNew();
+var score = 0;
+var possibilities = new Dictionary<string, int>()
+{
+    { "A X", 3 },
+    { "A Y", 4 },
+    { "A Z", 8 },
+    { "B X", 1 },
+    { "B Y", 5 },
+    { "B Z", 9 },
+    { "C X", 2 },
+    { "C Y", 6 },
+    { "C Z", 7 },
+};
+new List<string>(await File.ReadAllLinesAsync("input.txt")).ForEach(l =>
+{
+    score += possibilities.First(p => p.Key == l).Value;
+});
+watch.Stop();
+Console.WriteLine($"{score} in {watch.ElapsedMilliseconds}ms");
