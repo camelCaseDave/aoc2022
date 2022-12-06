9a73b5cd53c7617d0fc65a56981335422cd5c47e
Dave Clark <David.jb.clark@icloud.com>
2022-12-06 15:58:41 +0000

    day6, more linq trivialising

---
+++ day6/Program.cs
@@ -0,0 +1,12 @@
+﻿var input = File.ReadAllText("input.txt").ToCharArray();
+var marker = input.Take(14).ToList();
+for (var i = 0; i < input.Length; i++)
+{
+    if (marker.Distinct().Count() == 14)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+    marker.RemoveAt(0);
+    marker.Add(input[i]);
+}
