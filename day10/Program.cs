ff2bccdb8e43230a6ea4f5fe651ec206ab3432cd
Dave Clark <dave.clark@healthhero.com>
2022-12-10 08:24:03 +0000

    day10 p1

---
+++ day10/Program.cs
@@ -0,0 +1,15 @@
+﻿var (c, r, s) = (0, 1, 0);
+foreach (var l in File.ReadAllLines("input.txt"))
+{
+    cycle(0);
+    if (l != "noop")
+        cycle(int.Parse(l[5..]));
+}
+void cycle(int d)
+{
+    c++;
+    if (new[] { 20, 60, 100, 140, 180, 220 }.Contains(c))
+        s += c * r;
+    r += d;
+}
+Console.WriteLine(s);
